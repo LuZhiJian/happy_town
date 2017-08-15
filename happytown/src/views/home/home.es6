dbbd19367fbbabf3443430d129a3a9aa89f547ee
Luzhijian <739788147@qq.com>
2017-08-15 11:34:39 +0800

    fix 首页切图

--- happytown/src/views/home/home.es6
+++ happytown/src/views/home/home.es6
@@ -13,8 +13,33 @@ export default {
 
   data () {
     return {
-      title: '首页home',
-      id: 21
+      id: 21,
+      proList: [{
+        img: 'http://wx1.sinaimg.cn/mw690/9b7a7878ly1fi4eqyr2izj20go0bgju8.jpg',
+        id: 1,
+        name: '被遗忘的美好童年',
+        isCollected: false
+      }, {
+        img: 'http://wx1.sinaimg.cn/mw690/9b7a7878ly1fi4eqxtlnjj20go0b076j.jpg',
+        id: 2,
+        name: '去捕捉那时光',
+        isCollected: true
+      }, {
+        img: 'http://wx4.sinaimg.cn/mw690/9b7a7878ly1fi4equc964j20go0catbt.jpg',
+        id: 3,
+        name: '爷爷的故事',
+        isCollected: true
+      }, {
+        img: 'http://wx3.sinaimg.cn/mw690/9b7a7878ly1fi4equxr7jj20go0cdtc4.jpg',
+        id: 4,
+        name: '放飞的，都是初真',
+        isCollected: false
+      }, {
+        img: 'http://wx2.sinaimg.cn/mw690/9b7a7878ly1fi4eqvdsboj20go0butbk.jpg',
+        id: 5,
+        name: '放学了..',
+        isCollected: false
+      }]
     }
   },
 
--- happytown/src/views/home/home.vue
+++ happytown/src/views/home/home.vue
@@ -3,7 +3,34 @@
     <app-header :id="id"></app-header>
     <div class="home-content">
       <banner></banner>
-      <h4>{{title}}</h4>
+      <div class="index-list-wrap">
+        <ul class="index-tab-ul row">
+          <li class="flex-6 active">
+            <a href="#/">推荐</a>
+          </li>
+          <li class="flex-6">
+            <a href="#/">最热</a>
+          </li>
+        </ul>
+        <div class="product-list">
+          <div class="item" v-for="(v, i) in proList">
+            <a :href="'#/production/' + v.id">
+              <div class="img">
+                <img :src="v.img">
+              </div>
+              <div class="pro-name">{{v.name}}</div>
+              <div class="collection">
+                <button>
+                  <i class="icon-xin1" v-if="v.isCollected"></i>
+                  <i class="icon-xin" v-else></i>
+                  {{ v.isCollected ? '已收藏' : '收藏'}}
+                </button>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="no-more">没有更多了...</div>
+      </div>
     </div>
     <app-footer></app-footer>
   </div>
